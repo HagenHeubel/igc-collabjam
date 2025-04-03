72b2952b73016bcb53e2059c89b3ef6f4f720b17
protoplanetary-fish <94703814+Protoplanetary-Fish@users.noreply.github.com>
2025-04-03 21:56:22 +0200

    Tail now attaches
    Attaches via PhysicsBody2D as a sibling of what it attaches to

--- wip/wip-sqlpy/cat/line_2d.gd
+++ wip/wip-sqlpy/cat/line_2d.gd
@@ -6,14 +6,15 @@ extends Line2D
 @onready var tail_segment_3: RigidBody2D = $"../Tail segment3"
 @onready var tail_segment_4: RigidBody2D = $"../Tail segment4"
 @onready var tail_tip: Node2D = %TailTip
+@onready var tail: Node2D = $".."
 
-var curve : Curve2D = Curve2D.new()
 func _process(delta: float) -> void:
-	var p0 : Vector2 = static_body_2d.global_position
-	var p1 : Vector2 = tail_segment.global_position
-	var p2 : Vector2 = tail_segment_2.global_position
-	var p3 : Vector2 = tail_segment_3.global_position
-	var p4 : Vector2 = tail_segment_4.global_position
-	var p5 : Vector2 = tail_tip.global_position
-	var pts : Array[Vector2] = [p0, p1, p2, p3, p4, p5, p5]
+	rotation = -tail.rotation
+	var p0 : Vector2 = static_body_2d.global_position - tail.global_position
+	var p1 : Vector2 = tail_segment.global_position - tail.global_position
+	var p2 : Vector2 = tail_segment_2.global_position - tail.global_position
+	var p3 : Vector2 = tail_segment_3.global_position - tail.global_position
+	var p4 : Vector2 = tail_segment_4.global_position - tail.global_position
+	var p5 : Vector2 = tail_tip.global_position - tail.global_position
+	var pts : Array[Vector2] = [p0, p0, p1, p2, p3, p4, p5, p5]
 	points = Util.b_spline_from_points(pts, 20, 3)
