5b14ea0bfb175e4eddfdc4827b5d3c8de6b94177
protoplanetary-fish <94703814+Protoplanetary-Fish@users.noreply.github.com>
2025-04-09 22:16:05 +0200

    Minor fixes

--- wip/wip-staging/TowerComponents/1_TowerComponents/room_controller.gd
+++ wip/wip-staging/TowerComponents/1_TowerComponents/room_controller.gd
@@ -1,6 +1,8 @@
 class_name RoomController
 extends Node
 
+@export var print_room_transitions_to_console : bool = false
+
 var rooms: Array = []
 
 func _ready() -> void:
@@ -8,11 +10,19 @@ func _ready() -> void:
 	SignalBus.register_room.connect(_on_room_registered)
 	SignalBus.tower_ready.connect(_on_tower_ready)
 
+func _on_tower_ready() -> void:
+	_update_room_status()
+
+func _on_room_registered(room: TowerRoom) -> void:
+	rooms.append(room)
+
 func _on_room_changed(target_room: TowerRoom) -> void:
 	if not target_room:
 		print("Target room not found")
 		return
 	if target_room != GlobalVars.current_room:
+		if print_room_transitions_to_console:
+			prints("Room transition triggered from", GlobalVars.current_room.name, "to", target_room.name)
 		GlobalVars.current_room = target_room
 		_update_room_status()
 
@@ -27,9 +37,3 @@ func _get_active_rooms(target_room: TowerRoom) -> Array:
 	var active_rooms: Array = [target_room]
 	active_rooms.append_array(target_room.adjacent_rooms)
 	return active_rooms
-
-func _on_room_registered(room: TowerRoom) -> void:
-	rooms.append(room)
-
-func _on_tower_ready() -> void:
-	_update_room_status()
