74598e620de84d19cff88c034c2b53b206f1c5cb
HagenHeubel <h.heubel@gmx.net>
2025-04-10 22:41:41 +0200

    PouncePlayer: Small additions and code clean up
    duration of landing boost now exported, converted the wall and floor counters back to bools

--- wip/wip-hagen/rigidbody_controller/rigid_player_pounce.gd
+++ wip/wip-hagen/rigidbody_controller/rigid_player_pounce.gd
@@ -6,6 +6,7 @@ signal healed()
 signal died()
 
 @export var interaction_detect: InteractionDetector
+@export var animation_player: AnimationPlayer
 @export var visual_component :Node2D
 @export_group("Stats")
 @export var HEALTH_MAX: int = 9
@@ -26,7 +27,8 @@ signal died()
 @export var charge_always_full :bool = false ##If checked, pounce will always assume use maximum force
 @export_range(0.0,0.7) var min_jump_charge :float = 0.3 ##Charge starts at this fraction of jump force
 @export_range(0.1,0.9,0.05) var h_jump_control :float= 0.7 ##Multiplied with movement speed while airborne
-@export_range(0.5,3.0) var pounce_movement_boost :float = 1.5 
+@export_range(0.1,3.0) var landing_boost :float = 0.75 ##Factor that will be added on top of the character's speed upon landing a jump
+@export_range(0.1,4.0) var landing_boost_duration :float = 0.5 ##Duration in seconds of the character's speed boost upon landing
 @export_range(45,180.0,5.0) var pounce_aim_speed :float = 130.0 ##Aiming speed in Degrees per Second
 @export_range(5.0,40.0,0.5) var min_pounce_angle :float = 15.0
 @export_range(5.0,90.0,0.5) var def_pounce_angle :float = 45.0
@@ -43,9 +45,8 @@ var respawn_position = global_position
 
 var get_pounce_rotation_input :Callable
 var default_gravity_scale :float = self.gravity_scale
-var on_floor_count :int=false
-var on_wall_count :int=false
-var is_wall_left :bool=false
+var on_floor :bool=false
+var on_wall :bool=false
 var neutral_force :Vector2
 var jump_charge :float=min_jump_charge
 var current_movement_boost :float=0.0
@@ -59,6 +60,8 @@ func _ready() -> void:
 		push_error("No visual component assigned in player script ", self)
 	if !interaction_detect:
 		push_error("No interaction detector assigned in player script ", self)
+	if !animation_player:
+		push_error("No animation player assigned in player script ", self)
 	
 	#calculate gravity-neutralizing constant force 
 	neutral_force = Vector2(0.0,gravity*default_gravity_scale*-3.1)
@@ -66,8 +69,9 @@ func _ready() -> void:
 	constant_force = neutral_force + neutral_force*-1
 	#change degrees to radians for more efficient code
 	slope_angle_max = deg_to_rad(slope_angle_max)
-	#make charge time a fraction that can be multiplied with delta instead of a time in seconds
+	#convert values from seconds to fractions
 	charge_time = 1/charge_time
+	landing_boost_duration = 1/landing_boost_duration
 	#assign callable for pounce rotation input
 	match pounce_control_type:
 		0:
@@ -89,39 +93,38 @@ func _ready() -> void:
 
 ##Handles wall/floor detection
 func _integrate_forces(state: PhysicsDirectBodyState2D):
-	on_floor_count = 0
-	on_wall_count = 0
-	var floor_detected:bool=false
-	var wall_detected:bool=false
+	on_floor = false
+	on_wall = false
 	for i in range(state.get_contact_count()): 
 		if abs(state.get_contact_local_normal(i).angle_to(Vector2.UP)) <= (slope_angle_max*1.2):
-			floor_detected = true
-			on_floor_count += 1
+			on_floor = true
 		else:
-			wall_detected = true
-			on_wall_count += 1
-	on_floor_count = floor_detected
+			on_wall = true
 
 
 func _physics_process(delta: float) -> void:
 	var force :Vector2 = Vector2.ZERO
 	force.x = Input.get_axis("left", "right")
-	
+	if !is_zero_approx(force.x):
+		if !animation_player.is_playing():
+			animation_player.play(&"TestLibrary/anim_walk_test")
+	else:
+		if animation_player.is_playing():
+			animation_player.play(&"RESET")
 	#sets rotation target for gravity changes and rotation stabilizer
 	var rotation_target = clampf(calculate_avg_ground_normal(),-slope_angle_max,slope_angle_max)
 	
 	gravity_scale = default_gravity_scale
 	
 	#changes movement and gravity depending on floor/ground detection
-	if (on_floor_count==0):
+	if !on_floor:
 		constant_force = neutral_force + neutral_force*-1
 		force.x *= h_jump_control #reduce movement speed while airborne
-		if (on_wall_count>0): #slow down gravity while hanging on wall
+		if on_wall: #slow down gravity while hanging on wall
 			gravity_scale = default_gravity_scale * 0.7
 	else: #rotates gravity and tunes it down a little while on floor
 		constant_force = 0.4*(-neutral_force).rotated(rotation_target) + neutral_force 
-		current_movement_boost = max(current_movement_boost-pounce_movement_boost*delta*0.75, 0.0)
-		
+		current_movement_boost = max(current_movement_boost-landing_boost*landing_boost_duration*delta, 0.0)
 	
 	#Rotates towards the rotation target
 	if rotation > (rotation_target+0.3):
@@ -129,35 +132,31 @@ func _physics_process(delta: float) -> void:
 	elif rotation < (rotation_target-0.3):
 		apply_torque(5010.1*rotation_stabilizer)
 	
-	#Flips assigned 2D visuals
 	
 	#jump input handling
-	if (on_floor_count>0) and Input.is_action_pressed("jump"):
+	if on_floor and Input.is_action_pressed("jump"):
 		if abs(visual_component.rotation_degrees) < min_pounce_angle:
 			visual_component.rotation_degrees = def_pounce_angle*-visual_component.scale.x
 		visual_component.rotation_degrees = get_pounce_rotation_input.call(delta)
 		force.x = 0.0
 		jump_charge = minf(jump_charge+charge_time*delta,1.0)
 		visual_component.scale.y = 1.0-jump_charge*0.4
-	if (on_floor_count>0) and Input.is_action_just_released("jump"):
+	if on_floor and Input.is_action_just_released("jump"):
 		visual_component.scale.y = 1.0
 		#using the visual's scale.x to determine character direction
 		if charge_always_full:
 			jump_charge = 1.0
 		apply_impulse(Vector2.UP.rotated((PI*0.5-abs(visual_component.rotation))*visual_component.scale.x)*jump_impulse_strength*jump_charge)
 		visual_component.rotation = 0.0
-		current_movement_boost = pounce_movement_boost*jump_charge
+		current_movement_boost = landing_boost*jump_charge
 		jump_charge = min_jump_charge
 	
-	if (on_floor_count>0) and ((current_movement_boost<0.01)or(linear_velocity.y < 1.0)) and !Input.is_action_just_released("jump"): #only change sprite direction while jump isn't charging!
+	#Flips assigned 2D visuals
+	if on_floor and ((current_movement_boost<0.01)or(linear_velocity.y < 1.0)) and !Input.is_action_just_released("jump"): #only change sprite direction while jump isn't charging!
 		if force.x < -0.1:
 			visual_component.scale.x = -1.0
 		elif force.x > 0.1:
 			visual_component.scale.x = 1.0
-	#if (on_floor_count>0) and Input.is_action_just_pressed("jump"):
-		#apply_impulse(Vector2.UP.rotated(rotation_target*0.4)*jump_impulse_strength)
-	#elif (on_wall_count>0) and Input.is_action_just_pressed("jump"):
-		#apply_impulse(Vector2.UP.rotated(rotation_target)*jump_impulse_strength*0.55)
 	
 	#applied movement speed, rotates by ground normal and moves character
 	force.x *= move_force*(1.0+current_movement_boost)
