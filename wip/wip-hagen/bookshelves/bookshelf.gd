fb6de9857c64c9fa9eed851ab429035678a78ca6
HagenHeubel <h.heubel@gmx.net>
2025-04-15 19:12:24 +0200

    Magic Bookshelves - more changes

--- wip/wip-hagen/bookshelves/bookshelf.gd
+++ wip/wip-hagen/bookshelves/bookshelf.gd
@@ -15,8 +15,8 @@ class_name MagicBookshelf
 
 @export var book_width :float=10.0 ##Maximum width per book in pixels - used to determine amount of book slots
 @export_group("Book Leaving Checks")
-@export var book_interval :float= 5.0 ##Seconds between each check for if a book can leave
-@export_range(0.0,1.0,0.1) var book_chance :float= 0.5 ##Chance that a book will leave on an otherwise successful check
+@export var leaving_interval :float= 5.0 ##Seconds between each check for if a book can leave
+@export_range(0.0,1.0,0.1) var leaving_chance :float= 0.5 ##Chance that a book will leave on an otherwise successful check
 @export_group("References (Automatic)")
 @export var bookshelf_shape :RectangleShape2D
 @export var bookshelf_collision :CollisionShape2D
@@ -44,7 +44,8 @@ func _ready() -> void:
 			print("Creating shape for bookshelf: ", bookshelf_collision)
 			bookshelf_collision.shape = RectangleShape2D.new()
 		bookshelf_shape = bookshelf_collision.shape
-	timer.wait_time = book_interval
+	timer.wait_time = leaving_interval+randf()
+	timer.timeout.connect(_on_book_leaving_timer_timeout)
 	max_book_slots = bookshelf_shape.size.x/book_width
 	update_book_management()
 	#for i in range(max_book_slots):
@@ -52,11 +53,11 @@ func _ready() -> void:
 
 
 func _on_book_leaving_timer_timeout() -> void:
-	if randf() <= book_chance:
-		for book:FlyingBook in book_slots:
-			if book:
-				print("one book is leaving")
-				return
+	if book_slots.is_empty():
+		return
+	if randf() <= leaving_chance:
+		var book :FlyingBook= book_slots.pick_random()
+		remove_book_from_shelf(book)
 
 
 func can_store_book(book:FlyingBook)->bool:
@@ -70,86 +71,102 @@ func _on_child_entered_tree(node: Node) -> void:
 			return
 		await get_tree().process_frame
 		set_book_size(node)
-		node.global_position = get_position_of_best_slot(node)
 		sort_book_into_shelf(node)
-		print(get_position_of_best_slot(node))
 func _on_child_exiting_tree(node: Node) -> void:
 	if node is FlyingBook:
-		remove_book_from_shelf(node)
+		if Engine.is_editor_hint():
+			remove_book_from_shelf(node)
 
 
 func sort_book_into_shelf(new_book:FlyingBook):
-	var start_range :float= bookshelf_collision.position.x - (bookshelf_shape.size.x*0.5)
 	# If no other books exist yet, just put it in on the left
 	if book_slots.is_empty():
 		book_slots.append(new_book)
-		new_book.position.x = start_range+(new_book.width*0.5)
+		print(new_book)
+		print(bookshelf_collision)
+		new_book.position.x = bookshelf_collision.position.x - (bookshelf_shape.size.x*0.5) +(new_book.width*0.5)
 		new_book.position.y = bookshelf_collision.position.y + (bookshelf_shape.size.y*0.5) - (new_book.height*0.5)
 		return
 	# If other books exist, check for the largest open space
 	var max_space:float=0.0
-	var position:float=0.0
 	var slot:int=0
-	#ERROR books are being pushed too far for some reason
-	#ERROR the space freed up by the books isn't additive because only one book moves at a time (all the books between the latest book and the inserted book need to move)
 	for i in range(book_slots.size()):
 		print("checking book ",book_slots[i], " at index ", i)
-		print("starting loop with start_range ",start_range, " and max_space ", max_space)
 		if i == 0:
-			max_space = abs(start_range - (book_slots[i].position.x-(book_slots[i].width*0.5)))
-			position = start_range
-			start_range = book_slots[i].position.x+(book_slots[i].width*0.5)
-		else: #not first element
-			if max_space < (book_slots[i].position.x-(book_slots[i].width*0.5)-start_range):
-				max_space = book_slots[i].position.x-(book_slots[i].width*0.5)-start_range
-				position = start_range
-				slot = i
-			start_range = book_slots[i].position.x+(book_slots[i].width*0.5)
+			max_space = get_book_distance_to_left(0)
+			slot = 0
+		if max_space < get_book_distance_to_right(i):
+			max_space = get_book_distance_to_right(i)
+			slot = i+1
 		if max_space >= new_book.width:
 			break
-		if i == (book_slots.size()-1): # check behind last element too
-			print("checking space after last book")
-			print((0.5*bookshelf_shape.size.x-start_range))
-			if max_space < (0.5*bookshelf_shape.size.x-start_range):
-				max_space = 0.5*bookshelf_shape.size.x-start_range
-				position = start_range
-				slot = -1
+	
 	print("Max space found: ", max_space)
-	var index = 1
+	
+	var index = 0
 	var opened_space :float= 0.0
 	while(max_space < new_book.width):
 		var j = slot + index
 		var sn = signi(index)
-		if (j>0) and (j<(book_slots.size()-1)):
-			opened_space += abs(book_slots[j].position.x - book_slots[j+sn].position.x) - (book_slots[j].width*0.5) - (book_slots[j+sn].position.x)
-			book_slots[j].position.x += sn*(abs(book_slots[j].position.x - book_slots[j+sn].position.x) - (book_slots[j].width*0.5) - (book_slots[j+sn].position.x))
-		elif j==0:
-			opened_space -= (-bookshelf_shape.size.x*0.5 - (book_slots[0].position.x - book_slots[j].width*0.5))
-			book_slots[0].position.x += -bookshelf_shape.size.x*0.5 - (book_slots[0].position.x - book_slots[0].width*0.5)
-		elif j==(book_slots.size()-1):
-			opened_space += bookshelf_shape.size.x*0.5 - (book_slots[j].position.x + book_slots[j].width*0.5)
-			book_slots[j].position.x += bookshelf_shape.size.x*0.5 - (book_slots[j].position.x + book_slots[j].width*0.5)
+		if (j>=0) and (j<book_slots.size()):
+			if sn >= 0: #right
+				opened_space += get_book_distance_to_right(j)
+				var move_by :float= get_book_distance_to_right(j)
+				for i in range(abs(index+1)):
+					print("moving ",book_slots[slot+i].name, " to the right")
+					book_slots[slot+i].position.x += move_by
+			else:
+				opened_space += get_book_distance_to_left(j)
+				var move_by :float= get_book_distance_to_left(j)
+				for i in range(abs(index)):
+					print("moving ",book_slots[slot-i-1].name, " to the left")
+					book_slots[slot-i-1].position.x -= move_by
+		
 		index = Math.increment_int_alternating(index)
 		
 		if (max_space+opened_space) >= new_book.width:
 			break
-		if index > book_slots.size():
+		if index > (book_slots.size()*2):
 			push_error("Book sorting algorithm got trapped in an endless loop -> safety break activated")
 			break
 	
-	new_book.position.y = bookshelf_collision.position.y + (bookshelf_shape.size.y*0.5) - (new_book.height*0.5)
-	new_book.position.x = position+(new_book.width*0.5)
-	if slot >= 0:
+	if slot < book_slots.size():
+		new_book.position.x = get_book_edge(slot,-1) -(new_book.width*0.5)
 		book_slots.insert(slot,new_book)
 	else:
+		new_book.position.x = get_book_edge(slot-1,1) +(new_book.width*0.5)
 		book_slots.append(new_book)
+	new_book.position.y = bookshelf_collision.position.y + (bookshelf_shape.size.y*0.5) - (new_book.height*0.5)
+
 
 func remove_book_from_shelf(book:FlyingBook):
 	book_slots.erase(book)
+	if !Engine.is_editor_hint():
+		book.leave_bookshelf.call_deferred(self)
 
-func get_position_of_best_slot(book:FlyingBook)->Vector2:
-	var vec_out = bookshelf_collision.global_position
-	return vec_out
+
+##Returns positive float representing the distance to the left-next book or edge of the bookshelf
+func get_book_distance_to_left(i:int)->float:
+	var distance :float= 0
+	if i == 0:
+		distance = -bookshelf_shape.size.x*0.5 - get_book_edge(i,-1)
+	else:
+		distance = get_book_edge(i-1,1) - get_book_edge(i,-1)
+	return distance*-1
+##Returns positive float representing the distance to the right-next book or edge of the bookshelf
+func get_book_distance_to_right(i:int)->float:
+	var distance :float= 0
+	if i == (book_slots.size()-1):
+		distance = bookshelf_shape.size.x*0.5 - get_book_edge(i,1)
+	else:
+		distance = get_book_edge(i+1,-1) - get_book_edge(i,1)
+	return distance
+
+
+##Get left (-1) or right (1) edge of the book at index i
+func get_book_edge(i:int,left_right:int):
+	return book_slots[i].position.x + (book_slots[i].width*0.5*left_right)
+
 
 
 func get_remaining_space()->float:
@@ -170,5 +187,15 @@ func set_book_size(book:FlyingBook):
 	if (book.width<1.0) or (book.height<1.0):
 		for node in book.get_children(true):
 			if node is CollisionShape2D:
-				book.width = node.shape.get_rect().size.x
-				book.height = node.shape.get_rect().size.y
+				book.width = node.shape.get_rect().size.x * book.scale.x
+				book.height = node.shape.get_rect().size.y * book.scale.y
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is FlyingBook:
+		if body.target_bookshelf != self:
+			return
+		if !can_store_book(body):
+			body.find_bookshelf()
+			return
+		body.enter_bookshelf.call_deferred(self)
